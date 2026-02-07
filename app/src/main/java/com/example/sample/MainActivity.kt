f30478d442b2020338aa44b6a904db57b5556a29
gabriel_garcia <garthkoji.ma@gmail.com>
2026-02-07 12:52:09 +0800

    Trial fix on splash screen logic

--- app/src/main/java/com/example/sample/MainActivity.kt
+++ app/src/main/java/com/example/sample/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Text
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.sample.ui.theme.SAMPLETheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        setContentView(R.layout.fragment_home)
         setContent {
             SAMPLETheme {
                 Text(text = "App Loaded Successfully!")
