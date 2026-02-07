c16a6f2466b3f165c52da1a14d54462a2c8e6050
gabriel_garcia <garthkoji.ma@gmail.com>
2026-02-07 13:12:55 +0800

    Remove splash screen for testing of main home

--- app/src/main/java/com/example/sample/MainActivity.kt
+++ app/src/main/java/com/example/sample/MainActivity.kt
@@ -3,11 +3,9 @@ package com.example.sample
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.fragment_home)
