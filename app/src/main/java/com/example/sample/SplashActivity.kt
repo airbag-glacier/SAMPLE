e360ad662e5c3bbd6145c9d667ad4d3f5f106240
gabriel_garcia <garthkoji.ma@gmail.com>
2026-02-07 13:28:59 +0800

    Trail fix on splash screen logic

---
+++ app/src/main/java/com/example/sample/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.example.sample
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+
+            val intent = Intent(this, MainActivity::class.java)
+
+
+            startActivity(intent)
+
+
+            finish()
+
+        }, 7000) // 7000ms = 7 seconds
+    }
+}
