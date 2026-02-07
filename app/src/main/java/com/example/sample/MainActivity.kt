948528c3abeec721ea29eeddcbccf5c5f95b726b
gabriel_garcia <garthkoji.ma@gmail.com>
2026-02-07 13:00:43 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/sample/MainActivity.kt
+++ app/src/main/java/com/example/sample/MainActivity.kt
@@ -1,22 +1,15 @@
 package com.example.sample
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.example.sample.ui.theme.SAMPLETheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            SAMPLETheme {
-                Text(text = "App Loaded Successfully!")
-            }
-        }
+        setContentView(R.layout.fragment_home)
     }
 }
