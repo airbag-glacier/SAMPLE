4ec628ad94514758974964ef27af24eebadc09da
gabriel_garcia <garthkoji.ma@gmail.com>
2026-02-08 19:01:55 +0800

    TRIAL RUN OF APP LOGIC

--- app/src/main/java/com/example/sample/MainActivity.kt
+++ app/src/main/java/com/example/sample/MainActivity.kt
@@ -1,19 +1,28 @@
-package com.example.sample // Check your package name
+package com.example.sample
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        // 1. Install Splash Screen (Android 12+ style)
-        installSplashScreen()
-
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
 
-        // 2. LINK THE FRAME (activity_main), NOT THE PHOTO (fragment_home)
         setContentView(R.layout.activity_main)
+    }
+}
+
+class ScanFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // If you haven't created fragment_scan.xml yet, this line will be red.
+        // If it is red, create the layout file "fragment_scan" in res/layout.
+        return inflater.inflate(R.layout.fragment_scan, container, false)
     }
 }
