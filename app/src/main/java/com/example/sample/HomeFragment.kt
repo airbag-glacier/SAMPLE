5441faae0c4c4537346f0e7112f75e71e85475be
gabriel_garcia <garthkoji.ma@gmail.com>
2026-02-22 21:02:18 +0800

    Update HomeFragment.kt

--- app/src/main/java/com/example/sample/HomeFragment.kt
+++ app/src/main/java/com/example/sample/HomeFragment.kt
@@ -22,13 +22,18 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Find the Camera Button from your design
+        // 1. Setup Camera Button
         val btnCamera = view.findViewById<FloatingActionButton>(R.id.btnCamera)
-
-        // Make the button work
         btnCamera?.setOnClickListener {
             // Navigate to the Scan Screen
             findNavController().navigate(R.id.action_home_to_scan)
         }
-    }
+
+        // 2. Setup Checkup Card
+        val cardCheckup = view.findViewById<View>(R.id.cardCheckup)
+        cardCheckup.setOnClickListener {
+            // Navigate to the Checkup Screen
+            findNavController().navigate(R.id.action_homeFragment_to_checkupFragment)
+        }
+    } // <-- This is where the function should properly close
 }
