586a6fcdaefb6311ff58737c22aaa460ea80eabe
gabriel_garcia <garthkoji.ma@gmail.com>
2026-03-06 20:35:13 +0800

    Adding of BE-FAST education module
    Will fix it tomorrow

---
+++ app/src/main/java/com/example/sample/ui/theme/BefastFragment.kt
@@ -0,0 +1,30 @@
+package com.example.sample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class BefastFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_befast, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnBack = view.findViewById<ImageButton>(R.id.btnBack)
+        btnBack.setOnClickListener {
+
+            findNavController().popBackStack()
+        }
+    }
+}
